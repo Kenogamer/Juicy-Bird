c3144fe41b3eef0ccb5f7c16b5e51d89e8380a1d
LeBoiImmo <linnea.immo@elev.ga.lbs.se>
2019-09-16 10:51:48 +0200

    Initial Commit
    Initial Commit
    Påbörjat Player scriptet

---
+++ Juicy Bird/Assets/Scripts/GameControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    //public text scoreText;
+
+    public GameObject gameOvertext;
+
+    private int score = 0;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+    
+}
