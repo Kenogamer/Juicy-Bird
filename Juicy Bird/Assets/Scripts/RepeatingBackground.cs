980513a69b457cb67754652782f9c41f9fe65737
Kenogamer <55179371+Kenogamer@users.noreply.github.com>
2019-09-23 10:14:56 +0200

    background
    background

---
+++ Juicy Bird/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D groundcollider;
+    private float groundHlength;
+
+
+    void Start()
+    {
+        groundcollider = GetComponent<BoxCollider2D>();
+        groundHlength = groundcollider.size.x;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x < -groundHlength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundoffset = new Vector2(groundHlength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundoffset;
+    }
+}
---
+++ Juicy Bird/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float scrollSpeed = -1;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(scrollSpeed,0);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
