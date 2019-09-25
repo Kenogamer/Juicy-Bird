29446ad0b847756141e87d8977374987f8b8189a
LeBoiImmo <linnea.immo@elev.ga.lbs.se>
2019-09-25 08:54:38 +0200

    Initial Commit
    Initial Commit Scripts

---
+++ Juicy Bird/Assets/Scripts/MoveObstacles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacles : MonoBehaviour
+{       //Time to make the obstacles to move!
+
+    public float speed;     //so we can easy change the speed of the moving obstacles
+    
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
