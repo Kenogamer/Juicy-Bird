57d89891a8d6e377d042fcec7dd6859b2a49028b
Kenogamer <55179371+Kenogamer@users.noreply.github.com>
2019-09-25 08:56:12 +0200

    last commit
    last commit

---
+++ Juicy Bird/Assets/Scripts/BackgroundSwapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSwapper : MonoBehaviour
+{
+    public Camera Spring_Camera;
+    public Camera Winter_Camera;
+    public bool Ongoing;
+
+    void Start()
+    {
+        StartCoroutine(SwitchBackground());
+        Spring_Camera.enabled = false;
+        Winter_Camera.enabled = true;
+        
+    }
+
+    
+    void Update()
+    {
+       
+    }
+
+    IEnumerator SwitchBackground()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(30);
+            Spring_Camera.enabled = !Spring_Camera.enabled;
+            Winter_Camera.enabled = !Winter_Camera.enabled;
+        }
+
+    }
+
+}
---
+++ Juicy Bird/Assets/Scripts/OffsetScrolling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetScrolling : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer rend;
+    private Vector2 savedOffset;
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, 0);
+        rend.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+    }
+
+}
